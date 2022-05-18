cfafb1745f78cca64e038e3bd2dc91358f83fc14
Sibadyuti De <45902123+sibadyuti-de@users.noreply.github.com>
2022-05-18 22:17:49 +0530

    Update docker_installer.sh

--- docker_installer.sh
+++ docker_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-file=./docker.repo
-file2=/etc/yum.repos.d/
-cp -d $file $file2 ;
+file=./docker.repo ./basic.repo
+path=/etc/yum.repos.d/
+cp -d $file $path ;
 echo -e "\nfile copyied\n"
 # check network is connected or not if connected install some rpm packages used to install docker dependencys.
 if ping -c 1 -q goo.gl >&/dev/null; then
